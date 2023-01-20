42d9f7e38357636bf6bebcee448f2d59a4ffc89d
문경욱 <108646112+9776mk@users.noreply.github.com>
2023-01-20 10:26:44 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+
+def solution(numbers, direction):
+    answer = deque()
+
+    for i in numbers:
+        answer.append(i)
+
+    if direction == "right":
+        n = answer.pop()
+        answer.appendleft(n)
+
+    else:
+        n = answer.popleft()
+        answer.append(n)
+
+    return list(answer)

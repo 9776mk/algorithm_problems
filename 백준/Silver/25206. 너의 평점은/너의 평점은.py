3c5d3747f20e38818fd29f89f0ac81f33a1ad7f9
문경욱 <108646112+9776mk@users.noreply.github.com>
2023-03-01 20:01:11 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,30 @@
+N = 20
+sum_ = 0
+grade_ = {
+    "A+": 4.5,
+    "A0": 4.0,
+    "B+": 3.5,
+    "B0": 3.0,
+    "C+": 2.5,
+    "C0": 2.0,
+    "D+": 1.5,
+    "D0": 1.0,
+    "F": 0,
+}
+cnt_ = 0
+
+
+for i in range(N):
+    A, B, C = map(str, input().split())
+    if C == "P":
+        continue
+    else:
+        for key in grade_.keys():
+            if key == C:
+                sum_ += grade_[key] * (float(B))
+                cnt_ += float(B)
+
+if cnt_ != 0:
+    print(sum_ / cnt_)
+else:
+    print(0)
